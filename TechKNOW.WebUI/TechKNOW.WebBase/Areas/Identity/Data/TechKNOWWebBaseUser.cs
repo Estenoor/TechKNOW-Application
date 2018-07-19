5b74d54e89799df19f3882f7cf05faec90f4acea
SamzFerg <SamzFerg@gmail.com>
2018-07-19 13:56:00 -0500

    Initial Project Files
    Initial project files with Identity Scaffolding

---
+++ TechKNOW.WebUI/TechKNOW.WebBase/Areas/Identity/Data/TechKNOWWebBaseUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TechKNOW.WebBase.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the TechKNOWWebBaseUser class
+    public class TechKNOWWebBaseUser : IdentityUser
+    {
+    }
+}
---
+++ TechKNOW.WebUI/TechKNOW.WebBase/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechKNOW.WebBase.Data;
+
+[assembly: HostingStartup(typeof(TechKNOW.WebBase.Areas.Identity.IdentityHostingStartup))]
+namespace TechKNOW.WebBase.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
